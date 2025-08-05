445b9e44758a1cecccfad298018898d1faa2c633
avadhkheni <avadhkheni29@gmail.com>
2025-08-05 09:42:34 +0530

    the 90% server code is done login and logout test is remain

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(session({
   secret: 'ojefCCESQ8',
   resave: false,
   saveUninitialized: true,
-  cookie: { httpOnly: true, secure: false, sameSite: 'Lax' } // Set to true if using HTTPS
+  cookie: { httpOnly: true, secure: false, sameSite: 'Lax' } 
 }));
 
 connectDB();
@@ -17,9 +17,9 @@ const authRoutes = require('./src/auth/routes');
 const productRoutes = require('./src/products/routes');
 const userRoutes = require('./src/Users/routes');
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
